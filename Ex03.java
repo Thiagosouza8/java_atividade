b3be4110a847be07883fb08485b02a58323b0bfa
Thiagosouza8 <thiagosouza032018@gmail.com>
2023-02-27 22:52:46 -0300

    atividades

---
+++ Ex03.java
@@ -0,0 +1,18 @@
+public class Ex03 {
+}
+
+class Oi {
+
+    public static void main(String[] args) {
+        System.out.println("Oi!");
+    }
+
+}
+
+class Tchau {
+
+    public static void main(String[] args) {
+        System.out.println("Tchau!");
+    }
+
+}
---
+++ Ex07.java
@@ -0,0 +1,8 @@
+public class Ex07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world! 1");
+        System.out.println("Hello, world! 2");
+    }
+
+}
